21dcfd0b1a076194e1d3a41e1c08f343060efd4f
Bowเอง <60010980@kmitl.ac.th>
2018-04-26 00:42:20 +0700

    add final Value
    in dimensions health resources timing

--- src/me/zx96/piupiu/Resources.java
+++ src/me/zx96/piupiu/Resources.java
@@ -28,6 +28,7 @@ public class Resources {
     public static final String SPR_PROJECTILE_ENEMY_SMALL = SPR_BASE + "projectile_enemy_sm.png";
     public static final String SPR_PROJECTILE_ENEMY_LARGE = SPR_BASE + "projectile_enemy_lg.png";
     
+    public static final String SPR_ITEM_HEART = SPR_BASE + "heart.png";
     //Audio
     public static final String AUD_BGM = AUD_BASE + "bgm.mp3";
     public static final String AUD_SFX_EXPLOSION = AUD_BASE + "explode.mp3";
